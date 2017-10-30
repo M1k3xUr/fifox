6c420bbb8a6d30ae93340c77e46a09b35074a24a
M1k3xUr <maikelurquijo@yahoo.com>
2017-10-30 13:29:12 -0400

    Add files via upload

---
+++ dramafan/background.js
@@ -0,0 +1,10 @@
+// open a new tab and load my html page into it.
+function dramaPage() {
+    console.log("injecting");
+    browser.tabs.create({
+        "url": "/drama-page.html"
+    });
+}
+
+// add dramaPage() as a listener to clicks on the browser action
+browser.browserAction.onClicked.addListener(dramaPage);
